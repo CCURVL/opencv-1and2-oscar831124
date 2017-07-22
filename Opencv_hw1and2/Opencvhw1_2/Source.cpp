08f844a7e103b5f61fb47e15108f5f75f0670abe
oscar831124 <oscar831124@gmail.com>
2017-07-23 00:05:01 +0800

    Update Source.cpp

--- Opencv_hw1and2/Opencvhw1_2/Source.cpp
+++ Opencv_hw1and2/Opencvhw1_2/Source.cpp
@@ -18,7 +18,26 @@ int main() {
 
 int HW01_TestFunction() {
 
-	// Fill your answer here
+	int c;
+	string add= "D:\\test\\";
+	string add2;
+	vector<Mat> va;
+	Mat img;
+	for (c = 1; c <= 10; c++) {
+		std::stringstream ss;
+		ss << c;
+		string t = ss.str();	
+		add2 = add + t +".jpg";
+		cout << add2;
+		img = imread(add2);
+		va.push_back(img);
+		namedWindow("Display window", WINDOW_AUTOSIZE);
+		imshow("Display window", img);
+		waitKey(0);
+	}
+
+	
+	return 0;
 }
 
 int HW02_ReadAndShowData(int numberOfImgs) {
